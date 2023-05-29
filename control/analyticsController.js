917a9ff7f1259efc9304ca19e637440efc320cff
Dagim Asnake <dagimasnake1@gmail.com>
2023-05-28 18:19:03 -1200

    added recent sold and sold location

--- control/analyticsController.js
+++ control/analyticsController.js
@@ -2,6 +2,7 @@ const Product = require('../model/Product')
 const SECRET_KEY = "thisissecret"
 const jwt = require("jsonwebtoken")
 const wrapAsync = require('../util/wrapAsync')
+
 module.exports.getAnalytics = wrapAsync(async function (req, res) {
     const productAnalytics = await Product.aggregate([
         {
@@ -14,6 +15,44 @@ module.exports.getAnalytics = wrapAsync(async function (req, res) {
     ]);
     return res.json({
         msg: productAnalytics,
+    })
+        .status(200);
+});
+
+module.exports.getRecentProduct = async function (req, res) {
+    const Recents = await Product.find({ sold: true }).sort({ createdAt: -1 }).limit(5);
+    let data = [];
+    Recents.forEach((product) => {
+        let datas = {
+            _id: product.id,
+            productType: product.productType,
+            cost: product.cost,
+            productName: product.productName,
+            sold: product.sold,
+            soldBy: product.soldBy,
+            price: product.price,
+            location: product.location,
+            users: product.users,
+            soldDate: product.createdAt
+        }
+        data.push(datas);
+    });
+    return res.json({
+        msg: Recents
+    }).status(200)
+}
+
+module.exports.getLocation = wrapAsync(async function (req, res) {
+    const productLocation = await Product.aggregate([
+        {
+            $group: {
+                _id: "$location",
+                count: { $sum: 1 },
+            },
+        }
+    ]);
+    return res.json({
+        msg: productLocation,
     })
         .status(200);
 });
--- router/analyticRouter.js
+++ router/analyticRouter.js
@@ -4,5 +4,8 @@ const isUserAuth = require("../util/isUserAuth");
 const { isAdmin, isEmployee } = require("../util/Authorization")
 
 router.get('/', isUserAuth, analyticsController.getAnalytics)
+router.get('/recent', isUserAuth, analyticsController.getRecentProduct)
+router.get('/location', isUserAuth, analyticsController.getLocation)
+
 
 module.exports = router
