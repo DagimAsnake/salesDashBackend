8dde9c0c6acf319b77a9ed8d8ac566a92c3a559f
Dagim Asnake <dagimasnake1@gmail.com>
2023-06-14 06:15:56 -1200

    finished swagger

--- router/analyticRouter.js
+++ router/analyticRouter.js
@@ -12,6 +12,109 @@ const { isAdmin, isSales, isMarketing } = require("../util/Authorization")
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     RecentAnalyticsData:
+ *       type: object
+ *       properties:
+ *         productName:
+ *           type: string
+ *           description: The name of the product.
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: The creation date of the product.
+ *         totalAddedCost:
+ *           type: number
+ *           description: The total added cost of all sales for the product.
+ *       required:
+ *         - productName
+ *         - createdAt
+ *         - totalAddedCost
+ *
+ *   responses:
+ *     UnauthorizedError:
+ *       description: Unauthorized access error.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               msg:
+ *                 type: string
+ *                 description: The error message.
+ *             required:
+ *               - msg
+ *
+ *     InternalServerError:
+ *       description: Internal server error.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               msg:
+ *                 type: string
+ *                 description: The error message.
+ *             required:
+ *               - msg
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     AnalyticsData:
+ *       type: object
+ *       properties:
+ *         productName:
+ *           type: string
+ *           description: The name of the product.
+ *         totalAddedCost:
+ *           type: number
+ *           description: The total added cost of all sales for the product.
+ *         totalUsers:
+ *           type: number
+ *           description: The total number of users who purchased the product.
+ *         productType:
+ *           type: string
+ *           description: The type of the product.
+ *       required:
+ *         - productName
+ *         - totalAddedCost
+ *         - totalUsers
+ *         - productType
+ *
+ *   responses:
+ *     UnauthorizedError:
+ *       description: Unauthorized access error.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               msg:
+ *                 type: string
+ *                 description: The error message.
+ *             required:
+ *               - msg
+ *
+ *     InternalServerError:
+ *       description: Internal server error.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               msg:
+ *                 type: string
+ *                 description: The error message.
+ *             required:
+ *               - msg
+ */
+
+/**
+ * @swagger
  * /analytics:
  *   get:
  *     summary: Get product analytics
@@ -99,8 +202,83 @@ router.get('/recent', isUserAuth, analyticsController.getRecentProduct)
  */
 router.get('/location', isUserAuth, analyticsController.getLocation)
 
+/**
+ * @swagger
+ * /analytics/search:
+ *   get:
+ *     summary: Search for analytics data by product name.
+ *     description: Returns a list of analytics data for products matching the specified search term.
+ *     tags: [Analytics]
+ *     parameters:
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: The search term to filter products by name.
+ *     responses:
+ *       200:
+ *         description: A list of analytics data for products matching the search term.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/AnalyticsData'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
 router.get('/search', analyticsController.searchAnalytics);
 
+/**
+ * @swagger
+ * /analytics/searchrecent:
+ *   get:
+ *     summary: Search for recent analytics data by product name and date range.
+ *     description: Returns a list of recent analytics data for products matching the specified search term and within the specified date range.
+ *     tags: [Analytics]
+ *     parameters:
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: The search term to filter products by name.
+ *       - in: query
+ *         name: startDate
+ *         schema:
+ *           type: string
+ *           format: date
+ *         description: The start date of the date range to filter products by creation date.
+ *       - in: query
+ *         name: endDate
+ *         schema:
+ *           type: string
+ *           format: date
+ *         description: The end date of the date range to filter products by creation date.
+ *     responses:
+ *       200:
+ *         description: A list of recent analytics data for products matching the search term and within the date range.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: A flag indicating whether the operation was successful.
+ *                 recentProducts:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/RecentAnalyticsData'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
 router.get('/searchrecent', analyticsController.searchRecent)
 
 module.exports = router
--- router/salesRouter.js
+++ router/salesRouter.js
@@ -19,6 +19,100 @@ const { isAdmin, isSales, isMarketing } = require("../util/Authorization")
  *       properties:
  *         _id:
  *           type: string
+ *           description: The unique identifier of the product.
+ *         productName:
+ *           type: string
+ *           description: The name of the product.
+ *         sold:
+ *           type: number
+ *           description: The number of times the product has been sold.
+ *       required:
+ *         - _id
+ *         - productName
+ *         - sold
+ *
+ *   responses:
+ *     UnauthorizedError:
+ *       description: Unauthorized access error.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               msg:
+ *                 type: string
+ *                 description: The error message.
+ *             required:
+ *               - msg
+ *
+ *     InternalServerError:
+ *       description: Internal server error.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               msg:
+ *                 type: string
+ *                 description: The error message.
+ *             required:
+ *               - msg
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     SalesData:
+ *       type: object
+ *       properties:
+ *         productName:
+ *           type: string
+ *           description: The name of the product.
+ *         totalAddedCost:
+ *           type: number
+ *           description: The total added cost of all sales for the product.
+ *       required:
+ *         - productName
+ *         - totalAddedCost
+ *
+ *   responses:
+ *     UnauthorizedError:
+ *       description: Unauthorized access error.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               msg:
+ *                 type: string
+ *                 description: The error message.
+ *             required:
+ *               - msg
+ *
+ *     InternalServerError:
+ *       description: Internal server error.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               msg:
+ *                 type: string
+ *                 description: The error message.
+ *             required:
+ *               - msg
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
  *           description: The ID of the product
  *         productType:
  *           type: string
@@ -104,8 +198,68 @@ router.get('/', isUserAuth, salesController.productSales)
  */
 router.get('/popular', isUserAuth, salesController.popularProducts)
 
+/**
+ * @swagger
+ * /sales/popularsearch:
+ *   get:
+ *     summary: Search for popular products.
+ *     description: Returns a list of popular products based on the number of sales.
+ *     tags: [Sales]
+ *     parameters:
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: The search term to filter products by name.
+ *     responses:
+ *       200:
+ *         description: A list of popular products.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Product'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
 router.get('/popularsearch', isUserAuth, salesController.searchPopular)
 
+/**
+ * @swagger
+ * /sales/salesSearch:
+ *   get:
+ *     summary: Search for sales data by product name.
+ *     description: Returns a list of sales data for products matching the specified search term.
+ *     tags: [Sales]
+ *     parameters:
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: The search term to filter products by name.
+ *     responses:
+ *       200:
+ *         description: A list of sales data for products matching the search term.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/SalesData'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
 router.get('/salesSearch', isUserAuth, salesController.searchSales)
 
 module.exports = router
